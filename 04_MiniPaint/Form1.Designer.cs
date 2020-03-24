e40f2b385f9231325608582c9c1417cfa7fc8d42
Sebastian Wojczyk <sebastian.wojczyk@wmii.uni.lodz.pl>
2020-03-24 13:25:46 +0100

    Zrobiony wybór i rysowanie różnych kształtów (krzywa, linia prosta, prostokąt i elipsa).

--- 04_MiniPaint/Form1.Designer.cs
+++ 04_MiniPaint/Form1.Designer.cs
@@ -37,16 +37,23 @@
             this.zapiszJakoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.radioButtonCurve = new System.Windows.Forms.RadioButton();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.buttonColor = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDownWidth = new System.Windows.Forms.NumericUpDown();
             this.colorDialog = new System.Windows.Forms.ColorDialog();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxMyImage)).BeginInit();
             this.menuStrip.SuspendLayout();
             this.tableLayoutPanel1.SuspendLayout();
             this.panel1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).BeginInit();
             this.SuspendLayout();
@@ -80,6 +87,7 @@
             // plikToolStripMenuItem
             // 
             this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyToolStripMenuItem,
             this.otwórzToolStripMenuItem,
             this.zapiszJakoToolStripMenuItem});
             this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
@@ -117,6 +125,7 @@
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.groupBox2);
             this.panel1.Controls.Add(this.groupBox1);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(3, 3);
@@ -124,18 +133,101 @@
             this.panel1.Size = new System.Drawing.Size(144, 420);
             this.panel1.TabIndex = 1;
             // 
+            // radioButtonEllipse
+            // 
+            this.radioButtonEllipse.AutoSize = true;
+            this.radioButtonEllipse.Location = new System.Drawing.Point(10, 89);
+            this.radioButtonEllipse.Name = "radioButtonEllipse";
+            this.radioButtonEllipse.Size = new System.Drawing.Size(53, 17);
+            this.radioButtonEllipse.TabIndex = 3;
+            this.radioButtonEllipse.Text = "Elipsa";
+            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.radioButtonEllipse);
+            this.groupBox2.Controls.Add(this.radioButtonRectangle);
+            this.groupBox2.Controls.Add(this.radioButtonLine);
+            this.groupBox2.Controls.Add(this.radioButtonCurve);
+            this.groupBox2.Location = new System.Drawing.Point(0, 91);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(144, 116);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Kształt";
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(10, 66);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonRectangle.TabIndex = 2;
+            this.radioButtonRectangle.Text = "Prostokąt";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(10, 43);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(55, 17);
+            this.radioButtonLine.TabIndex = 1;
+            this.radioButtonLine.Text = "Prosta";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonCurve
+            // 
+            this.radioButtonCurve.AutoSize = true;
+            this.radioButtonCurve.Checked = true;
+            this.radioButtonCurve.Location = new System.Drawing.Point(10, 20);
+            this.radioButtonCurve.Name = "radioButtonCurve";
+            this.radioButtonCurve.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonCurve.TabIndex = 0;
+            this.radioButtonCurve.TabStop = true;
+            this.radioButtonCurve.Text = "Krzywa";
+            this.radioButtonCurve.UseVisualStyleBackColor = true;
+            // 
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.buttonColor);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Controls.Add(this.numericUpDownWidth);
-            this.groupBox1.Location = new System.Drawing.Point(-3, 4);
+            this.groupBox1.Location = new System.Drawing.Point(0, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(147, 108);
+            this.groupBox1.Size = new System.Drawing.Size(144, 81);
             this.groupBox1.TabIndex = 0;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Linia";
+            // 
+            // buttonColor
+            // 
+            this.buttonColor.BackColor = System.Drawing.Color.Black;
+            this.buttonColor.Location = new System.Drawing.Point(72, 47);
+            this.buttonColor.Name = "buttonColor";
+            this.buttonColor.Size = new System.Drawing.Size(72, 23);
+            this.buttonColor.TabIndex = 3;
+            this.buttonColor.UseVisualStyleBackColor = false;
+            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kolor";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Grubość";
             // 
             // numericUpDownWidth
             // 
@@ -160,33 +252,12 @@
             0});
             this.numericUpDownWidth.ValueChanged += new System.EventHandler(this.numericUpDownWidth_ValueChanged);
             // 
-            // label1
+            // nowyToolStripMenuItem
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(7, 20);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Grubość";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(7, 51);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(31, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Kolor";
-            // 
-            // buttonColor
-            // 
-            this.buttonColor.BackColor = System.Drawing.Color.Black;
-            this.buttonColor.Location = new System.Drawing.Point(72, 47);
-            this.buttonColor.Name = "buttonColor";
-            this.buttonColor.Size = new System.Drawing.Size(72, 23);
-            this.buttonColor.TabIndex = 3;
-            this.buttonColor.UseVisualStyleBackColor = false;
-            this.buttonColor.Click += new System.EventHandler(this.buttonColor_Click);
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
             // 
             // Form1
             // 
@@ -203,6 +274,8 @@
             this.menuStrip.PerformLayout();
             this.tableLayoutPanel1.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidth)).EndInit();
@@ -228,6 +301,12 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.NumericUpDown numericUpDownWidth;
         private System.Windows.Forms.ColorDialog colorDialog;
+        private System.Windows.Forms.RadioButton radioButtonEllipse;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.RadioButton radioButtonCurve;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
     }
 }
 
--- 04_MiniPaint/Form1.cs
+++ 04_MiniPaint/Form1.cs
@@ -24,11 +24,19 @@ namespace _04_MiniPaint
             myPen = new Pen(buttonColor.BackColor, (float)numericUpDownWidth.Value);
             myPen.EndCap = myPen.StartCap = System.Drawing.Drawing2D.LineCap.Round;
 
+            nowyToolStripMenuItem_Click(null, null);
+        }
+
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBoxMyImage.Image = new Bitmap(800, 600);
+            graphics = Graphics.FromImage(pictureBoxMyImage.Image);
+            graphics.Clear(Color.White);
         }
 
         private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            if(openFileDialog.ShowDialog() ==  DialogResult.OK)
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
             {
                 pictureBoxMyImage.Image = Image.FromFile(openFileDialog.FileName);
                 graphics = Graphics.FromImage(pictureBoxMyImage.Image);
@@ -59,14 +67,8 @@ namespace _04_MiniPaint
 
         private void pictureBoxMyImage_MouseDown(object sender, MouseEventArgs e)
         {
-            if(e.Button == MouseButtons.Left)
+            if (e.Button == MouseButtons.Left)
             {
-                //graphics.DrawEllipse(new Pen(Color.Red),
-                //                     e.X,
-                //                     e.Y,
-                //                     20,
-                //                     20);
-                //pictureBoxMyImage.Refresh();
                 tempPoint = e.Location;
             }
         }
@@ -74,16 +76,52 @@ namespace _04_MiniPaint
         private void pictureBoxMyImage_MouseMove(object sender, MouseEventArgs e)
         {
             if (e.Button == MouseButtons.Left)
-            { 
-                graphics.DrawLine(myPen, tempPoint, e.Location);
-                pictureBoxMyImage.Refresh();
+            {
+                if (radioButtonCurve.Checked)
+                {
+                    graphics.DrawLine(myPen, tempPoint, e.Location);
+                    pictureBoxMyImage.Refresh();
+                    tempPoint = e.Location;
+                }
             }
-            tempPoint = e.Location;
         }
 
         private void pictureBoxMyImage_MouseUp(object sender, MouseEventArgs e)
         {
-
+            if (e.Button == MouseButtons.Left)
+            {
+                if (radioButtonCurve.Checked)
+                {
+                    graphics.DrawLine(myPen, tempPoint, e.Location);
+                }
+                else if (radioButtonLine.Checked)
+                {
+                    graphics.DrawLine(myPen, tempPoint, e.Location);
+                }
+                else if (radioButtonRectangle.Checked)
+                {
+                    //wypełnienie
+                    //graphics.FillRectangle(new SolidBrush(Color.Green),
+                    //                       Math.Min(tempPoint.X, e.X),
+                    //                       Math.Min(tempPoint.Y, e.Y),
+                    //                       Math.Abs(tempPoint.X - e.X),
+                    //                       Math.Abs(tempPoint.Y - e.Y));
+                    graphics.DrawRectangle(myPen,
+                                           Math.Min(tempPoint.X, e.X),
+                                           Math.Min(tempPoint.Y, e.Y),
+                                           Math.Abs(tempPoint.X - e.X),
+                                           Math.Abs(tempPoint.Y - e.Y));
+                }
+                else if (radioButtonEllipse.Checked)
+                {
+                    graphics.DrawEllipse(myPen,
+                                         Math.Min(tempPoint.X, e.X),
+                                         Math.Min(tempPoint.Y, e.Y),
+                                         Math.Abs(tempPoint.X - e.X),
+                                         Math.Abs(tempPoint.Y - e.Y));
+                }
+                pictureBoxMyImage.Refresh();
+            }
         }
 
         private void numericUpDownWidth_ValueChanged(object sender, EventArgs e)
@@ -93,7 +131,7 @@ namespace _04_MiniPaint
 
         private void buttonColor_Click(object sender, EventArgs e)
         {
-            if(colorDialog.ShowDialog() == DialogResult.OK)
+            if (colorDialog.ShowDialog() == DialogResult.OK)
             {
                 buttonColor.BackColor = colorDialog.Color;
                 myPen.Color = colorDialog.Color;
